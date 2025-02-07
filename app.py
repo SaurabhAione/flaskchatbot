5b785fb8df7874d45befadbd55adbb1e3c9d29d3
SaurabhAione <saurabhaifirst@gmail.com>
2025-02-07 07:22:50 +0000

    s

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def get_response():
     return jsonify({"bot_response": bot_response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
